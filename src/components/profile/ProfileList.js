0cef4c7bc335165f57ddc93a99622e6767d34f15
Carter Culkin <carter.culkin@gmail.com>
2021-04-29 09:36:27 -0500

    fixing profilecard

--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ProfileCard } from './xProfileCard';
+import { ProfileCard } from './ProfileCard';
 import { getAllProfiles, remove } from '../../modules/ProfileManager';
 import { useHistory } from "react-router-dom";
 
